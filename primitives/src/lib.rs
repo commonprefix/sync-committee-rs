47a8ae29d3d595f4dfe65edad189c2adba675f42
themicp <themicp@gmail.com>
2023-11-09 14:51:11 +0200

    Always link alloc

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -1,6 +1,5 @@
 //! Primitive types for sync committee verifier
 //! This crate contains code adapted from https://github.com/ralexstokes/ethereum-consensus
-#![cfg_attr(not(feature = "std"), no_std)]
 #[warn(unused_imports)]
 #[warn(unused_variables)]
 extern crate alloc;
